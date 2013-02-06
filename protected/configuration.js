52ef9c4ce0cf17ecf018c92bbc0a9c7b2b679602
Web Deli™ <projects@webdeli.com.au>
2013-02-06 12:39:12 +1100

    Update configuration.js

--- protected/configuration.js
+++ protected/configuration.js
@@ -1,25 +1,27 @@
 /**
- * Created with htmlgo.
+ * Created with HTMLGO.
  * Author: Web Deli™
- * Date: 2012-11-14
- * Time: 1:36 PM
+ * Date: 2013-02-06
+ * Time: 12:30 PM
  *
  * @file
- * This is the configuration file for htmlgo.
+ * This is the configuration file for HTMLGO.
  */
 
-// JSON encoded configuration settings for htmlgo.
+// JSON encoded configuration settings for HTMLGO.
 
-// 1. Base URL [baseurl] - This is the main site URL for your site.
-// 2. WCAG [wcag] - Web Content Accessibility Guidelines settings.
+// 1. Base URL [baseurl]	-	This is the main site URL for your site.
+// 2. WCAG [wcag]			-	Web Content Accessibility Guidelines settings.
+// 3. WCAG [wcag][version]	-	WCAG major / minor version.
+// 4. WCAG [wcag][level]	-	WCAG confomance level (A, AA, AAA)
 
 {
     "baseurl" : "http:\\www.your-main-url.com",
-
     "wcag" : {
         "version" : "2.0",
         "level" : "A"
     }
 
 }
+
 // end config
